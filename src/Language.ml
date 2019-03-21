7bcdd3449138d12e9d83a094d9e86f0965f2df30
Bayarma <47855788+Bayarma@users.noreply.github.com>
2019-03-21 20:24:04 +0800

    Update Language.ml

--- src/Language.ml
+++ src/Language.ml
@@ -5,13 +5,13 @@ open GT
 
 (* Opening a library for combinator-based syntax analysis *)
 open Ostap.Combinators
-       
+
 (* Simple expressions: syntax and semantics *)
 module Expr =
   struct
-    
-    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
-       notation, it came from GT. 
+
+    (* The type for expressions. Note, in regular OCaml there is no "@type..."
+       notation, it came from GT.
     *)
     @type t =
     (* integer constant *) | Const of int
@@ -25,55 +25,51 @@ module Expr =
         +, -                 --- addition, subtraction
         *, /, %              --- multiplication, division, reminder
     *)
-                                                            
+
     (* State: a partial map from variables to integer values. *)
-    type state = string -> int 
+    type state = string -> int
 
     (* Empty state: maps every variable into nothing. *)
     let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-    (* Update: non-destructively "modifies" the state s by binding the variable x 
+    (* Update: non-destructively "modifies" the state s by binding the variable x
       to value v and returns the new state.
     *)
     let update x v s = fun y -> if x = y then v else s y
 
     (* Expression evaluator
-
           val eval : state -> t -> int
- 
-       Takes a state and an expression, and returns the value of the expression in 
+       Takes a state and an expression, and returns the value of the expression in
        the given state.
-    *)        
+    *)
+    let eval_binop op l r =
+      let bool_to_int b = if b then 1 else 0 in
+      match op with
+      | "+"  -> l + r
+      | "-"  -> l - r
+      | "*"  -> l * r
+      | "/"  -> l / r
+      | "%"  -> l mod r
+      | "<"  -> bool_to_int (l < r)
+      | ">"  -> bool_to_int (l > r)
+      | ">=" -> bool_to_int (l >= r)
+      | "<=" -> bool_to_int (l <= r)
+      | "==" -> bool_to_int (l = r)
+      | "!=" -> bool_to_int (l <> r)
+      | "&&" -> bool_to_int (l <> 0 && r <> 0)
+      | "!!" -> bool_to_int (l <> 0 || r <> 0)
+      | _    -> failwith "this operation is not supported"
 
-    let bool_of_int i = i != 0
+    let rec eval s e =
+      match e with
+      | Var v -> s v
+      | Const c -> c
+      | Binop (op, e1, e2) -> eval_binop op (eval s e1) (eval s e2)
 
-	let int_of_bool bool_check = if bool_check then 1 else 0
 
-	let get_op op left right = match op with
-  | "+" -> left + right
-  | "-" -> left - right
-  | "*" -> left * right
-  | "/" -> left / right
-  | "%" -> left mod right
-  | "!!" -> int_of_bool (bool_of_int left || bool_of_int right)
-  | "&&" -> int_of_bool (bool_of_int left && bool_of_int right)
-  | "==" -> int_of_bool (left == right)
-  | "!=" -> int_of_bool (left != right)
-  | "<=" -> int_of_bool (left <= right)
-  | "<" -> int_of_bool (left < right)
-  | ">=" -> int_of_bool (left >= right)
-  | ">" -> int_of_bool (left > right)
-
-let rec eval s expres = match expres with
-	    |Const c -> c 
-	    |Var v -> s v
-	    |Binop (op,l_e,r_e) -> get_op op (eval s l_e) (eval s r_e)
-       
     (* Expression parser. You can use the following terminals:
-
          IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
          DECIMAL --- a decimal constant [0-9]+ as a string
-                                                                                                                  
     *)
     let create_binop op = fun x y -> Binop(op, x, y)
     let parse_op_list ops = List.map (fun op -> ostap ($(op)), create_binop op) ops
